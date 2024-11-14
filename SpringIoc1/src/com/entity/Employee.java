9cfcb603976702629ef27ff32cad115f882386ee
Jeeni Solanki <jeenalsolanki91@gmail.com>
2024-11-14 14:09:41 +0530

    first commit

---
+++ SpringIoc1/src/com/entity/Employee.java
@@ -0,0 +1,8 @@
+package com.entity;
+
+public class Employee {
+	public void employeeDetails() {
+		System.out.println("employee called");
+	}
+
+}
---
+++ SpringIoc1/src/com/entity/Order.java
@@ -0,0 +1,8 @@
+package com.entity;
+
+public class Order {
+	public void orderDetail() {
+		System.out.println("order Details");
+	}
+
+}
